a6eb08e35e34b3b0fa343288dd5d253673ee884f
Man Ioan <man_ioan@live.com>
2022-05-28 21:39:12 +0300

    Add project files.

---
+++ Charity.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Charity.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
